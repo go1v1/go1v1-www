31b9efd6831e8221c55854e028d8c22a75237ddf
ngryman <ngryman@gmail.com>
2015-09-25 16:30:32 +0200

    env handling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var environment = {"baseUrl":"/"};(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports={
   "classic": {
     "name": "Classic",
@@ -1812,7 +1812,7 @@ var _pagesSummoner2 = babelHelpers.interopRequireDefault(_pagesSummoner);
 
 _go1v1LibRouter2['default'].add('/', new _pagesHome2['default']());
 _go1v1LibRouter2['default'].add('/summoner/:summoner', new _pagesSummoner2['default']());
-_go1v1LibRouter2['default'].start();
+_go1v1LibRouter2['default'].start({ base: environment.baseUrl });
 
 },{"::/pages/home":20,"::/pages/summoner":21,"babel-core/external-helpers":9,"go1v1-lib/router":7}],18:[function(require,module,exports){
 'use strict';
